5a995c89dfcaaf162a58458d2be5d1fb2edd6ee3
seunggeon <tmdrjs9555@naver.com>
2023-03-28 21:46:30 +0900

    Update Post.ts

--- mymodel/entities/Post.ts
+++ mymodel/entities/Post.ts
@@ -1,52 +1,52 @@
-// import {
-//   Column,
-//   Entity,
-//   Index,
-//   OneToMany,
-//   PrimaryGeneratedColumn,
-// } from "typeorm";
-// import { Comment } from "./Comment";
-// import { LikePost } from "./LikePost";
+import {
+  Column,
+  Entity,
+  Index,
+  OneToMany,
+  PrimaryGeneratedColumn,
+} from "typeorm";
+import { Comment } from "./Comment";
+import { LikePost } from "./LikePost";
 
-// @Index("Post_postIdx_uindex", ["postIdx"], { unique: true })
-// @Entity("Post", { schema: "immersion_DB" })
-// export class Post {
-//   @PrimaryGeneratedColumn({ type: "int", name: "postIdx" })
-//   postIdx: number;
+@Index("Post_postIdx_uindex", ["postIdx"], { unique: true })
+@Entity("Post", { schema: "immersion_DB" })
+export class Post {
+  @PrimaryGeneratedColumn({ type: "int", name: "postIdx" })
+  postIdx: number;
 
-//   @Column("int", { name: "writeIdx", nullable: true })
-//   writeIdx: number | null;
+  @Column("int", { name: "writeIdx", nullable: true })
+  writeIdx: number | null;
 
-//   @Column("varchar", { name: "category", nullable: true, length: 20 })
-//   category: string | null;
+  @Column("varchar", { name: "category", nullable: true, length: 20 })
+  category: string | null;
 
-//   @Column("varchar", { name: "title", nullable: true, length: 30 })
-//   title: string | null;
+  @Column("varchar", { name: "title", nullable: true, length: 30 })
+  title: string | null;
 
-//   @Column("varchar", { name: "content", nullable: true, length: 300 })
-//   content: string | null;
+  @Column("varchar", { name: "content", nullable: true, length: 300 })
+  content: string | null;
 
-//   @Column("timestamp", { name: "createdAt", nullable: true })
-//   createdAt: Date | null;
+  @Column("timestamp", { name: "createdAt", nullable: true })
+  createdAt: Date | null;
 
-//   @Column("timestamp", { name: "updatedAt", nullable: true })
-//   updatedAt: Date | null;
+  @Column("timestamp", { name: "updatedAt", nullable: true })
+  updatedAt: Date | null;
 
-//   @Column("mediumblob", { name: "image", nullable: true })
-//   image: Buffer | null;
+  @Column("mediumblob", { name: "image", nullable: true })
+  image: Buffer | null;
 
-//   @Column("varchar", { name: "fileName", nullable: true, length: 50 })
-//   fileName: string | null;
+  @Column("varchar", { name: "fileName", nullable: true, length: 50 })
+  fileName: string | null;
 
-//   @Column("int", { name: "likeNum", nullable: true })
-//   likeNum: number | null;
+  @Column("int", { name: "likeNum", nullable: true })
+  likeNum: number | null;
 
-//   @Column("int", { name: "viewNum", nullable: true })
-//   viewNum: number | null;
+  @Column("int", { name: "viewNum", nullable: true })
+  viewNum: number | null;
 
-//   @OneToMany(() => Comment, (comment) => comment.postIdx2)
-//   comments: Comment[];
+  @OneToMany(() => Comment, (comment) => comment.postIdx2)
+  comments: Comment[];
 
-//   @OneToMany(() => LikePost, (likePost) => likePost.postIdx2)
-//   likePosts: LikePost[];
-// }
+  @OneToMany(() => LikePost, (likePost) => likePost.postIdx2)
+  likePosts: LikePost[];
+}
