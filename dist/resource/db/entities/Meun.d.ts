c7fe62cb5f9e2af65863b51a04cfc6b4c42d0ab3
softmoca <acyc0909@gmail.com>
2023-08-26 21:01:09 +0900

    fix : meun_restaurant_DB

---
+++ dist/resource/db/entities/Meun.d.ts
@@ -0,0 +1,13 @@
+import { Restaurant } from "./Restaurant";
+export declare class Meun {
+    meunIdx: number;
+    restaurantIdx: number;
+    contentName: string | null;
+    price: number | null;
+    meunContent: string | null;
+    MeunViewNum: number | null;
+    createdAt: Date;
+    updatedAt: Date | null;
+    MeunImage: string | null;
+    restaurantIdx2: Restaurant;
+}
---
+++ resource/db/entities/Meun.ts
@@ -0,0 +1,52 @@
+import {
+  Column,
+  CreateDateColumn,
+  Entity,
+  Index,
+  JoinColumn,
+  ManyToOne,
+  PrimaryGeneratedColumn,
+  UpdateDateColumn,
+} from "typeorm";
+import { Restaurant } from "./Restaurant";
+
+@Index("Meun_meunIdx_uindex", ["meunIdx"], { unique: true })
+@Index("FK_Restaurant_TO_Meun_1", ["restaurantIdx"], {})
+@Entity("Meun", { schema: "immersion_DB" })
+export class Meun {
+  @PrimaryGeneratedColumn({ type: "int", name: "meunIdx" })
+  meunIdx: number;
+
+  @Column("int", { name: "restaurantIdx" })
+  restaurantIdx: number;
+
+  @Column("varchar", { name: "contentName", nullable: true, length: 100 })
+  contentName: string | null;
+
+  @Column("int", { name: "price", nullable: true })
+  price: number | null;
+
+  @Column("varchar", { name: "meunContent", nullable: true, length: 100 })
+  meunContent: string | null;
+
+  @Column("int", { name: "MeunViewNum", nullable: true })
+  MeunViewNum: number | null;
+
+  @CreateDateColumn()
+  createdAt: Date;
+
+  @UpdateDateColumn()
+  updatedAt: Date | null;
+
+  @Column("varchar", { name: "MeunImage", nullable: true })
+  MeunImage: string | null;
+
+  @ManyToOne(() => Restaurant, (restaurant) => restaurant.menus, {
+    onDelete: "NO ACTION",
+    onUpdate: "NO ACTION",
+  })
+  @JoinColumn([
+    { name: "restaurantIdx", referencedColumnName: "restaurantIdx" },
+  ])
+  restaurantIdx2: Restaurant;
+}
